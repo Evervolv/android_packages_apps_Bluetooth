15e52f6113001526a805060994ba4da0a2d74948
James Sullins <jcsullins@gmail.com>
2013-11-21 16:49:26 -0600

    btservice/AdaperState: handle ENABLED_READY in OffState
    If the adapter takes too long (more than ENABLE_TIMEOUT_DELAY) to enable,
    the AdapterState will go into the OffState. In this state, ENABLED_READY
    messages are ignored. When this happens, the bt vendor code thinks
    it's enabled, but the AdapterState doesn't. Thus, the two layers will
    stay out-of-sync until a reboot.
    
    Handling the ENABLED_READY message while in OffState seems to be a
    workable solution to the situation outlined above.
    
    Change-Id: I6d090440a085b433e50dbb7edee20eb1efa510de

--- src/com/android/bluetooth/btservice/AdapterState.java
+++ src/com/android/bluetooth/btservice/AdapterState.java
@@ -164,7 +164,12 @@ final class AdapterState extends StateMachine {
                case USER_TURN_OFF:
                    //TODO: Handle case of service started and stopped without enable
                    break;
-
+               case ENABLED_READY:
+                   if (DBG) Log.d(TAG,"CURRENT_STATE=OFF, MESSAGE = ENABLED_READY");
+                   mAdapterProperties.onBluetoothReady();
+                   transitionTo(mOnState);
+                   notifyAdapterStateChange(BluetoothAdapter.STATE_ON);
+                   break;
                default:
                    return false;
             }
